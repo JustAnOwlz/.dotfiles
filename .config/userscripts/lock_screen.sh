8bcd83e707d596f4f8be3ed5938bba95ed47a668
JustAnOwlz <urban97-luca@hotmail.it>
2019-04-14 00:29:47 +0200

    modified lockscreen behavior

--- .config/userscripts/lock_screen.sh
+++ .config/userscripts/lock_screen.sh
@@ -2,5 +2,6 @@
 
 maim -u /tmp/lock-screen.png
 convert /tmp/lock-screen.png -blur 5x5 -fill black -colorize 50% /tmp/lock-screen.png
-i3lock -i /tmp/lock-screen.png
+i3lock -f -i /tmp/lock-screen.png
 rm /tmp/lock-screen.png
+systemctl suspend
