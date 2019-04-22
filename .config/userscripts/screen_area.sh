c60a60c767e86f37690c517b931f93049f56347d
JustAnOwlz <urban97-luca@hotmail.it>
2019-04-22 13:14:49 +0200

    mpv, i3blocks and vim

--- .config/userscripts/screen_area.sh
+++ .config/userscripts/screen_area.sh
@@ -3,4 +3,4 @@
 maim -s --color=200,240,90,0.4 -l | convert - \
 	\( +clone -background black -shadow 80x3+5+5 \) +swap -background none \
 	-layers merge +repage \
-	~/Screenshots/$(date '+%Y-%m-%g--%H%M%S').png | xclip -selection clipboard
+	~/Screenshots/$(date '+%Y-%m-%d--%H%M%S').png | xclip -selection clipboard
--- .vimrc
+++ .vimrc
@@ -8,8 +8,11 @@ call vundle#end()            " required
 filetype plugin indent on    " required
 " see :h vundle for more details or wiki for FAQ
 
+
 set number relativenumber
 set ruler
 set cursorline
+set encoding=UTF-8
 
 syntax on
+
